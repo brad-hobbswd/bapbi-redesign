88a0a357fa13bb57de5b9a852a6be0f53a70a8a0
Bradley Hobbs <brad.w.hobbs@gmail.com>
2026-09-09 07:18:07 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
-java
+const mobileMenuButton =
+    document.getElementById("mobileMenuButton");
+
+const mobileNavigation =
+    document.getElementById("mobileNavigation");
+
+const mobileMenuClose =
+    document.getElementById("mobileMenuClose");
+
+
+if (
+    mobileMenuButton &&
+    mobileNavigation
+) {
+
+    mobileMenuButton.addEventListener(
+        "click",
+        () => {
+
+            mobileNavigation.classList.add(
+                "open"
+            );
+
+        }
+    );
+
+}
+
+
+if (
+    mobileMenuClose &&
+    mobileNavigation
+) {
+
+    mobileMenuClose.addEventListener(
+        "click",
+        () => {
+
+            mobileNavigation.classList.remove(
+                "open"
+            );
+
+        }
+    );
+
+}
